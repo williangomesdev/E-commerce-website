e0b361f36bdfa83745cf7bf79279457aabb35d75
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-16 19:51:57 -0300

    home page

---
+++ js/slides.js
@@ -0,0 +1,18 @@
+//Slide home section
+var swiper = new Swiper(".home", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
