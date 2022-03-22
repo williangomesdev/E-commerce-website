065ad33bfe6570f3bf89196707b39cb326be55b6
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-21 23:32:50 -0300

    index refatoring

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/shared'));
+
 
 app.get("/", function (req, res) {
     res.render("pages/index");
