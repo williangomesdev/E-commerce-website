8964ee1c3bf02e1632e45d31fc117d372ebddac6
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-22 01:00:49 -0300

    routes page configured

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const express = require("express");
-
-
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/shared'));
 
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/shared"));
 
 app.get("/", function (req, res) {
-    res.render("pages/index");
-  });
+  res.render("pages/index");
+});
+app.get("/sobre", function (req, res) {
+  res.render("pages/about");
+});
+app.get("/carrinho", function (req, res) {
+  res.render("pages/cart");
+});
+app.get("/contato", function (req, res) {
+  res.render("pages/contact");
+});
+app.get("/login", function (req, res) {
+  res.render("pages/login");
+});
+app.get("/produtos", function (req, res) {
+  res.render("pages/products");
+});
+app.get("/cadastro", function (req, res) {
+  res.render("pages/register");
+});
+
 
 app.listen(8080);
 console.log("Servidor rodando");
-
-
