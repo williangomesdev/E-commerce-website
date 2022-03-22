6433e993a1088029430855771fa3bd3fe3c0aac3
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-22 01:17:10 -0300

    create routes file

--- routes.js
+++ routes.js
@@ -1,7 +1,28 @@
-/* const express = require("express");
-const router = express();
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    res.render("pages/index");
+  });
+  app.get("/sobre", function (req, res) {
+    res.render("pages/about");
+  });
+  app.get("/carrinho", function (req, res) {
+    res.render("pages/cart");
+  });
+  app.get("/contato", function (req, res) {
+    res.render("pages/contact");
+  });
+  app.get("/login", function (req, res) {
+    res.render("pages/login");
+  });
+  app.get("/produtos", function (req, res) {
+    res.render("pages/products");
+  });
+  app.get("/cadastro", function (req, res) {
+    res.render("pages/register");
+  });
 
 
 
-module.exports = router;
- */
+module.exports = app;
--- server.js
+++ server.js
@@ -1,33 +1,12 @@
 const express = require("express");
 const app = express();
+const useRoute = require("./routes");
 
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/shared"));
-
-app.get("/", function (req, res) {
-  res.render("pages/index");
-});
-app.get("/sobre", function (req, res) {
-  res.render("pages/about");
-});
-app.get("/carrinho", function (req, res) {
-  res.render("pages/cart");
-});
-app.get("/contato", function (req, res) {
-  res.render("pages/contact");
-});
-app.get("/login", function (req, res) {
-  res.render("pages/login");
-});
-app.get("/produtos", function (req, res) {
-  res.render("pages/products");
-});
-app.get("/cadastro", function (req, res) {
-  res.render("pages/register");
-});
-
+app.use(useRoute);
 
 app.listen(8080);
 console.log("Servidor rodando");
